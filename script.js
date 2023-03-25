8e80ab39f73618a89ba6058c9f2c83a4fcf41dea
dallas eldridge <eldridgedallas@yahoo.com>
2023-03-24 22:38:07 -0700

    picture changes

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ let otherUserName= prompt ("Please enter with caution");
         let lionPics = prompt("How many times do you want lions? ")
         let parse =parseInt(lionPics)
         for (let i =0;i<parse; i++){
-            document.write('<img src=clement-roy-MUeeyzsjiY8-unsplash.jpg" alt="lions" />') 
+            document.write('<img src="lion12.jp"alt="lions" />') 
 
             
         }
@@ -50,5 +50,25 @@ let otherUserName= prompt ("Please enter with caution");
     guessAge()
 
 
+//  let displayBeach = prompt("How many flights outta here this year do you want? (Sending up to 5 planes to your location, beep boop)");
+//     displayBeach = parseInt(displayBeach);
+//     console.log(displayBeach);
+// while (displayBeach < 1 || displayBeach > 5){
+//     displayBeach = prompt ("Oop, slow down and follow directions! This is for science. Scale of 1-5, how much do you need a vacation? AND GO!");
+// }
 
-     
+// for (let i = 0; i < displayBeach; i++){
+//      document.write('<img class="tinyplane" src="tiny.plane.jpg" />');
+//      }
+
+
+    
+// return displayBeach;
+
+
+
+
+
+
+
+
