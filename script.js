5dac3b7c2d310efdc2a2437c4d1e4e6673148254
dallas eldridge <eldridgedallas@yahoo.com>
2023-03-25 17:45:44 -0700

    new commits

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 
 
 
-//if (Answer == ''){
+if (Answer == '');
 function getotheruserName (){
 let otherUserName= prompt ("Please enter with caution");
     document.write("Lions Are KINGS"+otherUserName)   
 }
 
-//if (anyAnswer == ''){
+if (anyAnswer == '');
     function getOtherOtherUserName (){
         let userName= prompt ("Guess what?");
         document.write("..YOU ARE AWESOME!!"+userName)   
     }
 
-//if (anyAnswer == 'Who CARES'){
+if (anyAnswer == 'Who CARES');
     function getuserName (){
         let userName= prompt ("Whats 55+3+2000+34323?");
         document.write("Welcome"+userName)   
@@ -30,24 +30,24 @@ let otherUserName= prompt ("Please enter with caution");
         let lionPics = prompt("How many times do you want lions? ")
         let parse =parseInt(lionPics)
         for (let i =0;i<parse; i++){
-            document.write('<img src="lion12.jp"alt="lions" />') 
+            document.write('<img src="lion12.jpeg"alt="lions" />') 
 
-            
-        }
+         }
+        return lionPics
     }
 
-    function guessAge(){
-        let userInput = parseInt(prompt("guess my age"))
-        while(userInput <23){
-            userInput = prompt ("guess too low, guess again.")
+    function greetUser()
+        let guessAge = parseInt(prompt("Guess my Age?"));
+        while(myAge <23){
+            myAge = prompt ("guess too low, guess again.")
        }
-       if (userInput=== 23){
-        alert ("Congratulations")
-       } else{
-        alert ("im not that old.")
+       if (guessAge=== 23){
+        alert="Congratulations"
+      }   else{ 
+        alert= ("im not that old.")
        }
-    }
-    guessAge()
+    
+    return guessAge;
 
 
 
